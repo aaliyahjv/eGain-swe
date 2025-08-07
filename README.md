b25b1d27f0fba93e79ffbab99cdca5a79a432538
aaliyahjv <aaliyahjv13@gmail.com>
2025-08-06 22:48:29 -0700

    add suggestion buttons, and approach and examples in README

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
 # egain-swe
 This repository contains my solution to the eGain SWE Take-Home Assignment. \
-Scenario: Helping a customer track a lost package.
+Scenario: Helping a customer track a lost package. \
+Language: HTML/CSS/JavaScript (basic web interface).
+
+## Setup/installation instructions
 
 
-Setup/installation instructions
-Brief explanation of your approach
-Screenshots/examples of the chatbot in action
+## Brief explanation of your approach
+**Goal:** \
+Implement a chatbot that addresses users' problems quickly and easily in a more natural language than other AI bots.
+
+**Implementation and design steps:** \
+1. Research - experience a chatbot of another company, and ask it to find a lost package.
+2. Design simple chatbot conversation flow using research and interests of self and others.
+3. Start coding!
+⋅⋅⋅⋅1. HTML: add DOCTYPE, head with meta, title, and links, body with chat container (including chat box and input row), chat button, and suggestion buttons (very last thing added as it was of less importance compared to other components), in that order.
+⋅⋅⋅⋅2. CSS: style to get a sense of what the chatbot looks like before testing with input. Ended with styling after making sure Javascript code was functioning correctly.
+⋅⋅⋅⋅3. JavaScript: save elements from HTML, create variables, create sendMessage function that uses user input, create appendMessage function that displays messages correctly, create botMessage function that controls the chatbot's conversation flow, add code that starts the conversation, add event listener for chat button, and add suggestMessage function and suggestion button arrays, in that order.
+
+## Screenshots/examples of the chatbot in action
+Chat button displayed on website \
+![alt text](img/display-chat-button "Viewing chat button") \
+
+Opening the chatbot \
+![alt text](img/opening-chat "Opening the chatbot") \
+
+Sending a message that doesn't contain a keyword \
+![alt text](img/without-keyword "Sending message without keyword") \
+
+Using suggestion buttons to fill user's text input \
+![alt text](img/suggest-button "Fill input with suggestion button") 
+
+Sending a message that contains keyword "track" or "lost" (conversation starts to flow) \
+![alt text](img/with-keyword "Sending message with keyword") \
+
+Sending an invalid order number (less than 6 digits, and 6 digits but not in list of orders) \
+![alt text](img/invalid-order-num3 "Sending 3-digit order number") \
+![alt text](img/invalid-order-num6 "Sending invalid 6-digit order number") \
+
+Sending a valid order number (6 digits and contained in list of orders) \
+![alt text](img/valid-order-num "Sending valid order number") \
--- script.js
+++ script.js
@@ -4,19 +4,17 @@
  *      chat container when chat button is on "even" click (2nd, 4th, 6th 
  *      click) using toggle. 
  *  chatBox - add new messages, and allow for scrolling in the chat box.
- *  userInput - text input sent by user.
- */
+ *  userInput - text input sent by user. */
 const chatButton = document.getElementById("chat-button");
 const chatContainer = document.getElementById("chat-container");
 const chatBox = document.getElementById("chat-box");
 const userInput = document.getElementById("user-input");
 
 /** Save the following elements:
- * current date.
- * map of orders for testing of different order numbers and estimated 
+ *  current date - using the Date() function.
+ *  orders - map of orders for testing of different order numbers and estimated 
  *      delivery dates.
- * steps for conversation tracking.
- */
+ *  step - steps for conversation tracking. */
 const currentDate = new Date();
 const currDateString = currentDate.toString();
 const orders = new Map([
@@ -33,9 +31,36 @@ chatButton.addEventListener("click", () => {
 });
 
 /** Function that:
- *  Takes the user's input and removes any whitespace at the ends. If an
- *      empty message is sent, the conversation does not continue (bot does
- *      not respond).
+ *  Saves suggestions-container element from HTML to know where to display
+ *      suggestion buttons.
+ *  Clear's existing suggestion buttons before getting replaced.
+ *  For every string in the array of suggestions
+ *      A new button is created.
+ *      The button is given a className and the string as it's text.
+ *  Gives the ability to fill in the user's input text with the button's text
+ *      when the button is clicked. Then the user can edit the text or press 
+ *      enter. 
+ *  Adds the suggestion button to the suggestions container. */
+function suggestMessages(updatedSuggestions) {
+    const userSuggestions = document.querySelector('.suggestions-container');
+    userSuggestions.innerHTML = "";
+
+    updatedSuggestions.forEach(text => {
+        const btn = document.createElement('button');
+        btn.className = 'suggestion-btn';
+        btn.textContent = text;
+        btn.onclick = () => {
+            document.getElementById('user-input').value = text;
+            document.getElementById('user-input').focus();
+        };
+        userSuggestions.appendChild(btn);
+    });
+}
+
+/** Function that:
+ *  Takes the user's input and removes any whitespace at the ends. 
+ *      If an empty message is sent, the conversation does not continue (bot
+ *      does not respond).
  *  Clears the user's input value after displaying message to chat (appending
  *      done in appendMessage function). 
  *  Waits 500ms for the bot to send a response. */
@@ -51,11 +76,10 @@ function sendMessage() {
   }, 500);
 }
 
-/** Function that
+/** Function that:
  *  Checks who is sending the message to set the correct message alignment.
  *  Adds the message to the corresponding message bubble.
- *  Allows for scrolling of the chat box.
-*/
+ *  Allows for scrolling of the chat box. */
 function appendMessage(sender, message) {
     const wrapperDiv = document.createElement("div");
     wrapperDiv.className = sender === "user" ? "message-wrapper user-align" : 
@@ -69,14 +93,26 @@ function appendMessage(sender, message) {
     chatBox.appendChild(wrapperDiv);
 
     chatBox.scrollTop = chatBox.scrollHeight;
+
 }
 
-/** Function that controls the chatbot's conversation flow using keyword-based
- *      recognition. */
+/** Function that:
+ *  Controls the chatbot's conversation flow using keyword-based
+ *      recognition. 
+ *  Changes the user's message to all lower caseto handle all strings equally. 
+ *  Is initialized at step 0 at beginning of code.
+ * 
+ *  If a certain keyword is used by the user, the chatbot will know which
+ *      problem to address, and will send the corresponding response. 
+ *  For every step in the conversation flow, the value of step changes. */
 function botMessage(userMessage) {
     const msg = userMessage.toLowerCase();
 
-    // Problem initializing flow
+    /** Problem initializing flow.
+     *  Asking for the return policy only involves one step by the 
+     *      chatbot, so step does not change. However, problems like tracking
+     *      order or return an order would change the step, as the conversation
+     *      flow would continue. */
     if (step === 0) {
         if (msg.includes("return policy")) {
             appendMessage("bot", "Yes, of course. Here is our return policy: Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur facere maiores vel id quos, nisi veniam dolorum? Perspiciatis accusantium laboriosam laudantium earum ex nobis aperiam debitis. Adipisci, deleniti reprehenderit error debitis vitae consectetur, officia alias consequatur aliquid doloremque eveniet, animi qui?");
@@ -84,24 +120,40 @@ function botMessage(userMessage) {
                 appendMessage("bot", "Let me know if there is anything else I can help you with!");
             }, 1000);
         } else if (msg.includes("return")) {
-            appendMessage("bot", "Sorry to here that you're thinking about making a return. If you've changed your mind, we'll do our best to make things right!, Otherwise, I can get the return started for you. Can you please send me the order number?");
-            // EXAMPLE: step = 2;
+            suggestMessages([]);
+            appendMessage("bot", "Sorry to here that you're thinking about making a return. I can get the return started for you. Can you please send me the order number?");
         } else if (msg.includes("track") || msg.includes("lost") ||
                 msg.includes("missing") || msg.includes("status")) {
+            suggestMessages([]);
             appendMessage("bot", "Sure thing! Let's start tracking your package. Can you please send me the order number?");
             step = 1;
         } else if (msg.includes("near") || msg.includes("nearest") ||
                 msg.includes("closest") || msg.includes("store") ||
+                msg.includes("nearby") || msg.includes("close by") ||
                 msg.includes("location")) {
             appendMessage("bot", "We can't wait to have you! Can you please send me the city you're looking to shop at?");
-            // EXAMPLE: step = 3; 
+            /* Suggest cities based on user's current location (extra step to
+                    ask user for permission to share location), or don't
+                    include suggestion buttons. */
+            suggestMessages(["Sunnyvale", "Mountain View", "Santa Clara"]);
         }
         return;
     }
 
-    /**Package tracking flow 
-     * Additionally comments added for improvement with more information.
-    */
+    /** Package tracking flow. 
+     *  If the user's input is an existing order number, check the estimated
+     *      delivery date.
+     *      The chatbot will send the order's status whether the estimated
+     *          date is earlier, on, or later than the current date. 
+     *      If the estimated delivery date is earlier than or on the current 
+     *      date (and time), the package should've been delivered. The 
+     *      chatbot will double check that it has arrived.
+     *          Step will change.
+     *      Else, the chatbot will say to wait for the package's delivery.
+     *  Else, the chatbot will suggest to double check the digits of user's
+     *      input, or send an email the order is a associated with.
+     *      
+     *  Additional comments added for improvement given more information. */
     if (step === 1) {
         const userTrackingNum = msg;
         if ((orders.has(userTrackingNum))) {
@@ -116,6 +168,7 @@ function botMessage(userMessage) {
                         on ${estDeliveryString} at the 
                         ${orders.get(userTrackingNum)[1]}. Has it arrived?`);
                 }, 1000);
+                suggestMessages(["Yes", "No"]);
                 step = 11;
             } else {
                 appendMessage("bot", `Thanks! Checking the status of order 
@@ -128,14 +181,17 @@ function botMessage(userMessage) {
                             following morning, please feel free to check in 
                             with me again!`);
                 }, 1000);
+                suggestMessages(["Make a return", "Find a store nearby", "Track my lost package", "Return policy"]);
                 step = 0;
             }
         } else if (msg.includes("@")) {
             let userEmail = msg;
+            suggestMessages([]);
             appendMessage("bot", `Thanks! Checking our system for orders 
                     associated with the email ${userEmail} ...`);
-            // Continue to find order number in database and help customer 
+            // IMPROVE: Continue to find order number in database and help customer 
         } else {
+            suggestMessages([]);
             appendMessage("bot", `Thanks! Checking the status of order number 
                     ${userTrackingNum} ...`);
             setTimeout(() => {
@@ -144,19 +200,26 @@ function botMessage(userMessage) {
         } 
     }
 
-    // Potential escalation flow.
+    /** Potential escalation flow.
+     *  If the package has arrived, the bot will send a closing.
+     *  Else, the bot will ask if the user wants to return or replace the 
+     *      package. */
     if (step === 11) {
         if (msg.includes("yes")) {
             appendMessage("bot", "Great! Enjoy your order!");
-            step = 1;
+            suggestMessages(["Make a return", "Find a store nearby", "Track my lost package", "Return policy"]);
+            step = 0;
         } else if (msg.includes("no")) {
             appendMessage("bot", "Oh no! I hope we can make it up to you! Would you like to return or replace this order?");
+            suggestMessages(["Return", "Replace"]);
+            step = 12;
         }
     }
-    // Continue with replacement or return process 
+    // IMPROVE: Continue with replacement or return process 
 }
 
 // Starts the conversation when the chat container loads.
 window.onload = () => {
   appendMessage("bot", "Oh, hi! My name is Bob Bot, but you can call me Bob. What can I help you with?");
+  suggestMessages(["Make a return", "Find a store nearby", "Track my lost package", "Return policy"]);
 };
